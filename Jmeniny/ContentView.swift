edc4f44de00dda1f62f0b7403972bf03c02498d4
Jaroslav Hejný <jaroslav.hejny@ringier.sk>
2025-03-15 00:48:31 +0100

    Initial Commit

---
+++ Jmeniny/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jmeniny
+//
+//  Created by Jaroslav Hejný on 15/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jmeniny/JmeninyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JmeninyApp.swift
+//  Jmeniny
+//
+//  Created by Jaroslav Hejný on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct JmeninyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
